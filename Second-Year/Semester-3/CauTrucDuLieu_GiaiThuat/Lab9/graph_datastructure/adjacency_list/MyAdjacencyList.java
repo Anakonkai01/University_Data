babd85c515067516cda03ebe53dab8292ab8e465
Anakonkai <nguyentranhoangnhan18@gmail.com>
2024-11-11 17:33:54 +0700

    chua lam xong adj lít

---
+++ Second-Year/Semester-3/CauTrucDuLieu_GiaiThuat/Lab9/graph_datastructure/adjacency_list/MyAdjacencyList.java
@@ -0,0 +1,52 @@
+package graph_datastructure.adjacency_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdjacencyList {
+    private List<List<Integer>> adjListUnweighted;
+    private List<List<Edge>> adjListWeighted;
+    private boolean isWeighted;
+
+    private static class Edge{
+        int destination;
+        int weight;
+        public  Edge(int destination, int weight){
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    public MyAdjacencyList(int numVertices){
+        isWeighted = false;
+        adjListUnweighted = new ArrayList<>();
+        for(int i = 0; i < numVertices; i++){
+            adjListUnweighted.add(new ArrayList<>());
+        }
+    }
+
+    public MyAdjacencyList(int numVertices, boolean isWeighted){
+        this.isWeighted = isWeighted;
+        if(isWeighted == true){
+            adjListWeighted = new ArrayList<>();
+            for(int i = 0; i < numVertices; i++){
+                adjListWeighted.add(new ArrayList<>());
+            }
+        }
+    }
+
+
+    public void addEdgeUnweightedUndirected(int source, int destination){
+        if(!isWeighted){
+            adjListUnweighted.get(source).add(destination);
+            adjListUnweighted.get(destination).add(source);
+        }
+    }
+
+    public void addEdgeWeightedUndirected(int source, int destination){
+        if(!isWeighted){
+            adjListWeighted.get(source).add(destination);
+            adjListWeighted.get(destination).add(source);
+        }
+    }
+}
