16cce325939eef62c9f863200d3b840c97a88c3d
Anakonkai01 <nguyentranhoangnhan18@gmail.com>
2024-11-16 12:33:01 +0700

    thuc hien xong lab 6, lab 7 chua lam bai tap
    thuc hien xong MaxHeap lab8, chua lam cac class khac da dc cai dang trong lab8 + chua thuc hien bai tap lab 8

--- Second-Year/Semester-3/CauTrucDuLieu_GiaiThuat/RevisionDSA/Revision_Final_Term/src/Lab6_Revision/MyBTS_Tree/MyBTS_Tree.java
+++ Second-Year/Semester-3/CauTrucDuLieu_GiaiThuat/RevisionDSA/Revision_Final_Term/src/Lab6_Revision/MyBTS_Tree/MyBTS_Tree.java
@@ -1,9 +1,14 @@
 package Lab6_Revision.MyBTS_Tree;
 
+import com.sun.jdi.IntegerType;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyBTS_Tree {
 
     // insert new node
-    public Node insert(Node node, int data){
+    public static Node insert(Node node, int data){
         if(node == null){
             return new Node(data);
         }
@@ -19,7 +24,7 @@ public class MyBTS_Tree {
 
 
     // pre-order NLR
-    public void preOrder_NLR(Node node){
+    public static void preOrder_NLR(Node node){
         if(node == null){
             return;
         }
@@ -29,17 +34,17 @@ public class MyBTS_Tree {
     }
 
     // in-order LNR
-    public void inOrder_LNR(Node node){
+    public static void inOrder_LNR(Node node){
         if(node == null){
             return;
         }
-        inOrder_NLR(node.getLeft());
+        inOrder_LNR(node.getLeft());
         System.out.print(node.getData() + " ");
-        inOrder_NLR(node.getRight());
+        inOrder_LNR(node.getRight());
     }
 
     // post-order LRN
-    public void postOrder_LNR(Node node){
+    public static void postOrder_LNR(Node node){
         if(node == null){
             return;
         }
@@ -49,7 +54,7 @@ public class MyBTS_Tree {
     }
 
     // search data
-    public Node search(Node node, int data){
+    public static Node search(Node node, int data){
         if(node == null){
             return null;
         }
@@ -67,15 +72,15 @@ public class MyBTS_Tree {
     }
 
     // find min
-    public int getMin(Node node){
+    public static Node getMin(Node node){
         if(node.getLeft() == null){
-            return node.getData();
+            return node;
         }
         return getMin(node.getLeft());
     }
 
     // find max
-    public int getMax(Node node){
+    public static int getMax(Node node){
         if(node.getRight() == null){
             return node.getData();
         }
@@ -84,23 +89,151 @@ public class MyBTS_Tree {
 
 
     // create tree by passing a string
-    public static Node createTree(String str){
-        return  null;
+    public static Node createTree(Node root,String str){
+        String [] keys = str.split(" ");
+        for(String s : keys){
+            MyBTS_Tree.insert(root, (Integer.parseInt(s)));
+        }
+        return root;
     }
 
     // contains
+    public static boolean contains(Node node, int data){
+        if(node == null){
+            return false;
+        }
+        if(node.getData() == data){
+            return true;
+        }
+        if(node.getData() < data){
+            return contains(node.getLeft(), data);
+        }
+        else if(node.getData() > data){
+            return contains(node.getRight(), data);
+        }
+        return false;
+    }
+
     // deleteMax
+    public static Node deleteMax(Node node){
+        if(node.getRight()== null){
+            return node.getLeft();
+        }
+        return deleteMax(node.getRight());
+    }
     // deleteMin
+    public static Node deleteMin(Node node){
+        if(node.getLeft()== null){
+            return node.getRight();
+        }
+        return deleteMin(node.getLeft());
+    }
+
     // deleteNode
+    public static Node deleteNode(Node node, int data){
+        if(node == null){
+            return null;
+        }
+        // tim node
+        if(data < node.getData()){
+            node.setLeft(deleteNode(node.getLeft(), data));
+        }
+        else if(data > node.getData()){
+            node.setRight(deleteNode(node.getRight(), data));
+        }
+        else{
+            if(node.getLeft() == null && node.getRight() == null){
+                return null;
+            }
+            if(node.getLeft() == null){
+                return node.getRight();
+            }
+            if(node.getRight() == null){
+                return node.getLeft();
+            }
+
+            Node successor = MyBTS_Tree.getMin(node.getRight());
+            node.setData(successor.getData());
+            node.setRight(deleteNode(node.getRight(), successor.getData()));
+        }
+        return node;
+    }
+
+    // find successor
+
     // delete predecessor
     // delete successor
-    // find height of the tree
-    // sum key in tree
-    // sum leave
-    // sum even
-    // count leaves
-    // sum even leave
-    // bfs
 
+
+    // find height of the tree
+    public static int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = 1 + height(node.getLeft());
+        int right = 1 + height(node.getRight());
+
+        return Math.max(left, right);
+    }
+    // sum key in tree
+    public static int sum(Node node) {
+        if(node == null){
+            return 0;
+        }
+        return node.getData() + sum(node.getLeft()) + sum(node.getRight());
+    }
+    // sum leave
+    public static int sumLeaves(Node node) {
+        if(node == null){
+            return 0;
+        }
+        if(node.getLeft() == null && node.getRight() == null){
+            return node.getData();
+        }
+        return sumLeaves(node.getLeft()) + sumLeaves(node.getRight());
+    }
+    // sum even
+    public static int sumEven(Node node) {
+        if(node == null){
+            return 0;
+        }
+        if(node.getData() % 2 == 0){
+            return node.getData() + sumEven(node.getLeft()) + sumEven(node.getRight());
+        }
+        return sumEven(node.getLeft()) + sumEven(node.getRight());
+    }
+    // count leaves
+    public static int countLeaves(Node node){
+        if(node == null){
+            return 0;
+        }
+        if(node.getLeft() == null && node.getRight() == null){
+            return 1 + countLeaves(node.getLeft()) + countLeaves(node.getRight());
+        }
+        return countLeaves(node.getLeft()) + countLeaves(node.getRight());
+    }
+    // bfs
+    public static void bfs_print(Node node){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            node = queue.poll();
+            System.out.print(node.getData() + " ");
+            if(node.getLeft() != null){
+                queue.add(node.getLeft());
+            }
+            if(node.getRight() != null){
+                queue.add(node.getRight());
+            }
+        }
+    }
+
+    // check validBTSTree
+//    public static boolean isValidBTS(Node node, Integer min, Integer max){
+//        if(node == null){
+//            return true;
+//        }
+//        if(node.getLeft())
+//    }
 
 }
